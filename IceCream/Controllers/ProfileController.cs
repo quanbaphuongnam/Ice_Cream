4ebc37cb571c7493e08c0f609cbc09d2f568fb09
quanbaphuongnam <nam.qbp29@aptecchlearning.edu.vn>
2021-07-02 16:37:19 +0700

    nam updaet

--- IceCream/Controllers/ProfileController.cs
+++ IceCream/Controllers/ProfileController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IceCream.Models;
+using IceCream.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +13,35 @@ namespace IceCream.Controllers
     [Route("profile")]
     public class ProfileController : Controller
     {
-        [Route("profile")]
-        [Route("")]
-        public IActionResult Index()
+        public DatabaseContext db;
+
+        private ProfileService profileService;
+
+        private IWebHostEnvironment webHostEnvironment;
+        public ProfileController(DatabaseContext _db, ProfileService _profileService, IWebHostEnvironment _webHostEnvironment)
         {
+           
+            db = _db;
+            profileService = _profileService;
+            webHostEnvironment = _webHostEnvironment;
+        }
+
+        [Route("profile/{id}")]
+        [Route("")]
+        public IActionResult Index(int id)
+        {
+
+            if (HttpContext.Session.GetInt32("account") == id)
+            {
+                ViewBag.profile = from a in db.Accounts
+                                  where a.AccId == id
+                                  select a;
+            }
+            else
+            {
+                return View("~/Views/Home/Page404.cshtml");
+            }
+           
             return View("profile");
         }
     }
--- IceCream/Startup.cs
+++ IceCream/Startup.cs
@@ -42,6 +42,7 @@ namespace IceCream
             services.AddScoped<RecipeService, RecipeServiceImpl>();
             services.AddScoped<SavourService, SavourServiceImpl>();
             services.AddScoped<AccountService, AccountServiceImpl>();
+            services.AddScoped<ProfileService, ProfileServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
