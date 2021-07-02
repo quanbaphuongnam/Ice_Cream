0353ebd509c0c6178b6ce7a1999e8710b22f2e68
quanbaphuongnam <nam.qbp29@aptecchlearning.edu.vn>
2021-07-02 20:49:17 +0700

    nam update

--- IceCream/Controllers/ProfileController.cs
+++ IceCream/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
 ﻿using IceCream.Models;
+using IceCream.Models.BigModel;
 using IceCream.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -36,6 +37,49 @@ namespace IceCream.Controllers
                 ViewBag.profile = from a in db.Accounts
                                   where a.AccId == id
                                   select a;
+                ViewBag.accountrecipe = (from f in db.Formulas                                       
+                                                join a in db.Accounts on f.ForContributors equals a.AccId
+                                         where (f.ForContributors
+                                                == id)
+                                                select new RecipeDetail
+                                                {
+                                                    AccId = a.AccId,
+                                                    AccUsername = a.AccUsername,
+                                                    AccAvatar = a.AccAvatar,
+                                                    ForId = f.ForId,
+                                                    ForCover = f.ForCover,
+                                                    ForName = f.ForName,
+                                                    ForDescription = f.ForDescription,
+                                                    ForCondition = f.ForCondition,
+                                                    ForStatus = f.ForStatus,
+                                                    ForCreated = f.ForCreated,
+                                                   
+                                                    //ConLai = (x.Soluong - y.Soluong)
+
+                                                }).ToList();
+                ViewBag.accountfeedback = (from f in db.Formulas
+                                                join fb in db.FeedbackFormulas on f.ForId equals fb.FormulaId
+                                                join a in db.Accounts on fb.AccId equals a.AccId
+                                                where (a.AccId == id)
+                                                select new RecipeDetail
+                                                {
+                                                    AccId = a.AccId,
+                                                    AccUsername = a.AccUsername,
+                                                    AccAvatar = a.AccAvatar,
+                                                    ForId = f.ForId,
+                                                    ForCover = f.ForCover,
+                                                    ForName = f.ForName,
+                                                    ForDescription = f.ForDescription,
+                                                    ForCondition = f.ForCondition,
+                                                    ForStatus = f.ForStatus,
+                                                    ForCreated = f.ForCreated,
+                                                    FeedbackId = fb.FeedbackId,
+                                                    Content = fb.Content,
+                                                    Created = fb.Created,
+                                                    FeedbackStatus = fb.FeedbackStatus
+                                                    //ConLai = (x.Soluong - y.Soluong)
+
+                                                }).ToList();
             }
             else
             {
