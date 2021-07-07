ba9d907d7d8c3088a9483f6bcd0acd6389ead4c3
tranngocbao128 <bao0924327813@gmail.com>
2021-07-07 23:39:39 +0700

    a

---
+++ IceCream/Controllers/Admin/ManageInvoiceController.cs
@@ -0,0 +1,45 @@
+﻿using IceCream.Models;
+using IceCream.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCream.Controllers.Admin
+{
+    [Route("manageinvoice")]
+    public class ManageInvoiceController : Controller
+    {
+        public DatabaseContext db;
+       
+        private ManageInvoiceServices manageInvoiceServices;
+        private InvoiceDetailAccountService invoiceDetailAccountService;
+
+        public ManageInvoiceController(DatabaseContext _db, ManageInvoiceServices _manageInvoiceServices, InvoiceDetailAccountService _invoiceDetailAccountService)
+        {
+            db = _db;
+
+            manageInvoiceServices = _manageInvoiceServices;
+            invoiceDetailAccountService = _invoiceDetailAccountService;
+        }
+
+        [Route("manageinvoice")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewBag.manageInvoice = manageInvoiceServices.FindAll();
+            return View("manageinvoice");
+        }
+        [Route("detail")]
+        public IActionResult Detail(string id)
+        {
+
+            ViewBag.detail = from a in db.InvoiceDetailAccounts
+                              where a.InvId == id
+                              select a;
+            return View("detail");
+        }
+
+    }
+}
---
+++ IceCream/Services/ManageInvoiceServicesImpl.cs
@@ -0,0 +1,22 @@
+﻿using IceCream.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCream.Services
+{
+    public class ManageInvoiceServicesImpl : ManageInvoiceServices
+    {
+        public DatabaseContext db;
+        public ManageInvoiceServicesImpl(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        public List<InvoiceAccount> FindAll()
+        {
+            return db.InvoiceAccounts.ToList();
+        }
+    }
+}
